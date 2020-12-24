6881d08cbb7472b225cc14665241d823a522d736
Jagadees <jagatheeshthangaraj@gmail.com>
2020-12-24 12:53:24 +0530

    Test push

--- server.py
+++ server.py
@@ -7,7 +7,7 @@ class Handler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
         self.send_response(HTTPStatus.OK)
         self.end_headers()
-        self.wfile.write(b'Hello world')
+        self.wfile.write(b'Hello D')
 
 
 httpd = socketserver.TCPServer(('', 1998), Handler)
